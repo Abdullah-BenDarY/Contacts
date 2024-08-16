baf56235bdad28e954d60f2bda8b29ddd6aa1b72
AbdullahBendary <abdullahbendary7@gmail.com>
2024-08-16 03:19:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/contacts/ui/DetailsFragment.kt
@@ -0,0 +1,10 @@
+package com.example.contacts.ui
+
+
+import com.example.contacts.base.BaseFragment
+import com.example.contacts.databinding.FragmentDetailsBinding
+
+class DetailsFragment : BaseFragment<FragmentDetailsBinding>(FragmentDetailsBinding::inflate) {
+
+
+}
---
+++ app/src/main/java/com/example/contacts/ui/HomeFragment.kt
@@ -0,0 +1,10 @@
+package com.example.contacts.ui
+
+import com.example.contacts.base.BaseFragment
+import com.example.contacts.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+
+}
+
